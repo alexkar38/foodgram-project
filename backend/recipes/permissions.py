65a799523473aae2dce248ead618ef2315455854
Алексей Карпов <a01alex@yandex.ru>
2022-08-08 21:13:11 +0300

    ну вроже работает

--- backend/recipes/permissions.py
+++ backend/recipes/permissions.py
@@ -8,13 +8,3 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
             or obj.author == request.user
         )
 
-
-class IsAuthorAdminOrReadOnly(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.user.is_authenticated and (
-            request.user.is_superuser
-            or obj.author == request.user
-            or request.method == "POST"
-        ):
-            return True
-        return request.method in permissions.SAFE_METHODS
